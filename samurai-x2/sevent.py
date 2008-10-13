605f4dbe8aaf2417f45cf4c35ec6412beef968bd
jochen.maes <jochen.maes@561c22c7-a851-0410-91f3-c76db74392ff>
2008-10-13 19:46:55 +0000

    partially completed they keyevents added timer: when no valid events for more than 10 seconds exit
    git-svn-id: https://samurai-x.googlecode.com/svn/trunk@49 561c22c7-a851-0410-91f3-c76db74392ff

--- samurai-x2/sevent.py
+++ samurai-x2/sevent.py
@@ -2,6 +2,9 @@
 Input monitoring for key events and mouse (press/release) events. 
 Mouse movements are not captured.
 '''
+import sys
+import time
+
 import samuraix.xcb as xcb
 
 # get connection to the xcb
@@ -25,19 +28,36 @@ w = xcb.window.Window.create(c, screen, 10, 10, 200, 200, 10,        \
                                 })
 w.map()
 gc = xcb.graphics.GraphicsContext.create(c, w, {'foreground':screen.black_pixel})
+old_time = int(time.time())
+def check_timer():
+    cur_time = int(time.time())
+    if (cur_time - old_time) > 10:
+        print '10 seconds exceeded, exiting...'
+        return False
+    return True
 
 @w.event
 def on_expose(evt):
     print 'on_expose %s'%(evt)
 
 def on_key_event(evt):
-    print '%(event)s, serial %(keycode)s, window %(window)s' %({'event':evt, 'keycode':evt.keycode, 'window':evt.event._xid})
-
+    from samuraix.xcb.keysymbols import KeySymbols
+    from samuraix.xcb.keylookup import  keysym_to_str
+    keysymbols = KeySymbols(c)
+    keysym = keysymbols.get_keysym(evt.keycode)
+    keystring = keysym_to_str(keysym)
+    print '%(event)s, serial %(keycode)s, window 0x%(window)X,'% \
+        ({'event':evt, 'keycode':evt.keycode, 'window':evt.event._xid})
+    print 'root 0x%(root)X, time %(time)d, (%(win_x)d,%(win_y)d), root:(%(root_x)d,%(root_y)d),'% \
+        ({'root':evt.root._xid,'time':evt._event.time,'win_x':evt.event_x, 'win_y':evt.event_y, 'root_x':evt.root_x, 'root_y':evt.root_y})
+    print 'state 0x%(state)X, keycode %(keycode)d (keysym 0x%(keysymbol)X, "%(keystring)s")), '% \
+        ({'state':evt.state, 'keycode':evt.keycode, 'keysymbol':keysym, 'keystring':keystring})
 event_map = {xcb.event.KeyPressEvent.event_type: on_key_event, \
             xcb.event.KeyReleaseEvent.event_type: on_key_event, \
             }
-
-while 1:
-    evt = c.wait_for_event()
-    if evt.event_type in event_map:
-        event_map[evt.event_type](evt)
+while check_timer():
+    evt = c.poll_for_event()
+    if evt:
+        if evt.event_type in event_map:
+            old_time = int(time.time())
+            event_map[evt.event_type](evt)
