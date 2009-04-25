54ef4c99dd9c201f084c958d2cfba423d13be87e
Friedrich Weber <fred@samurai-x.org>
2009-04-25 18:14:50 +0200

    ooxcb: nicer xtest.fake_input

--- ooxcb/examples/xtesttest.py
+++ ooxcb/examples/xtesttest.py
@@ -7,24 +7,17 @@ sys.path.append('..')
 import ooxcb
 import ooxcb.xproto
 import ooxcb.xtest
+from ooxcb.constant import KeyPress, KeyRelease
+from ooxcb.keysymdef import XK_B
 
 conn = ooxcb.connect()
 
-with conn.bunch():
-    conn.xtest.fake_input(2,
-            38,
-            0,
-            ooxcb.xproto.XNone,
-            0,
-            0,
-            0
-    )
+# fetch the keycode of the 'b' key
+keycode = conn.keysyms.get_keycode(XK_B)
 
-    conn.xtest.fake_input(3,
-            38,
-            0,
-            ooxcb.xproto.XNone,
-            0,
-            0,
-            0
-    )
+with conn.bunch():
+    # simulate a 'b' key press
+    conn.xtest.fake_input(KeyPress, keycode)
+    # simulate a 'b' key release (necessary)
+    conn.xtest.fake_input(KeyRelease, keycode)
+
--- ooxcb/xtest.i
+++ ooxcb/xtest.i
@@ -2,4 +2,13 @@ ExternallyWrapped:
     - WINDOW
     - CURSOR
 
+Requests:
+    FakeInput:
+        defaults:
+            time: 0
+            window: XNone
+            rootX: 0
+            rootY: 0
+            deviceid: 0
+
 # vim: ft=yaml
