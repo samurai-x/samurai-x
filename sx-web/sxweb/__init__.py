9edeacffaf6eab2ae800dd0f57284747d49f760c
dunk <dunk@inspo.(none)>
2009-05-27 17:11:42 +0100

    made the sx-web a bit more usefull - you can see the config and run actions now

--- sx-web/sxweb/__init__.py
+++ sx-web/sxweb/__init__.py
@@ -1,15 +1,11 @@
 from samuraix.plugin import Plugin
+import samuraix
 import BaseHTTPServer
-import cgi, random, sys
+import cgi, sys
+from pprint import pformat
 
-MESSAGES = [
-    "That's as maybe, it's still a frog.",
-    "Albatross! Albatross! Albatross!",
-    "It's Wolfgang Amadeus Mozart",
-    "A pink form from Reading.",
-    "Hello people, and welcome to 'It's a Tree'"
-    "I simply stare at the brick and it goes to sleep.",
-]
+import logging
+log = logging.getLogger(__name__)
 
 
 class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
@@ -21,23 +17,33 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        try:
-            # redirect stdout to client
-            stdout = sys.stdout
-            sys.stdout = self.wfile
-            self.makepage()
-        finally:
-            sys.stdout = stdout # restore
-        
+        self.wfile.write(self.makepage())
+
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile, 
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+
+        samuraix.app.plugins['actions'].emit(form['action'].value, {})        
+        self.do_GET()
+
     def makepage(self):
-        # generate a random message
-        tagline = random.choice(MESSAGES)
-        print "<html>"
-        print "<body>"
-        print "<p>Today's quote: "
-        print "<i>%s</i>" % cgi.escape(tagline)
-        print "</body>"
-        print "</html>"
+        return """\
+<html>
+<body>
+<h1>Config</h1>
+<pre>%(config)s</pre>
+<h1>Run Action</h1>
+<form method='post'>
+    <input type='text' name='action'/>
+    <input type='submit' value='run action'/>
+</form>
+""" % {
+        'config': pformat(samuraix.config),
+        }
 
 
 class SXWeb(Plugin):
@@ -47,6 +53,7 @@ class SXWeb(Plugin):
         app.push_handlers(self)
 
         PORT = 8000
+
         self.httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
     
         app.add_fd_handler('read', self.httpd.socket, self.httpd.handle_request)
