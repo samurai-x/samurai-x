f691e4671508091657276a1e485c15463f1bba26
dunk <dunk@inspo.(none)>
2009-06-05 22:48:10 +0100

    use webob

--- sx-web/sxweb/__init__.py
+++ sx-web/sxweb/__init__.py
@@ -8,6 +8,25 @@ from wsgiref import simple_server
 import logging
 log = logging.getLogger(__name__)
 
+from webob import Request, Response
+from webob import exc
+
+class App(object):
+    def __call__(self, environ, start_response):
+        req = Request(environ)
+        if req.environ['REMOTE_ADDR'] != '127.0.0.1':
+            resp = exc.HTTPBadRequest(str(e))
+        try:
+            resp = self.process(req)
+        except ValueError, e:
+            resp = exc.HTTPBadRequest(str(e))
+        except exc.HTTPException, e:
+            resp = e
+        return resp(environ, start_response)
+
+    def process(self, request):
+        return Response(content_type="text/html", body="hello!")
+
 
 class SXWeb(Plugin):
 
@@ -15,10 +34,12 @@ class SXWeb(Plugin):
         self.app = app
         app.push_handlers(self)
 
-        PORT = 8000
+        self.config = samuraix.config.get('sxweb', {})
+        self.port = self.config.get('port', 8000)
 
-        self.httpd = simple_server.make_server('', PORT, simple_server.demo_app)
-    
+    def on_ready(self, app):
+        log.info('serving on port %s', self.port)
+        self.httpd = simple_server.make_server('', self.port, App())
         app.add_fd_handler('read', self.httpd.socket, self.httpd.handle_request)
 
 
