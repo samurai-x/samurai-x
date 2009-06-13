32a0ea5111fcb3219792d7b4335162fab88adc0d
dunk <dunk@inspo.(none)>
2009-06-13 17:16:07 +0100

    sx-web docs

--- samurai-x2/docs/source/plugins/sx-web.rst
+++ samurai-x2/docs/source/plugins/sx-web.rst
@@ -5,12 +5,6 @@
 sx-web
 =============
 
-sx-web is a plugin that runs a wsgi server inside samurai-x. 
-
-
-API documentation
------------------
-
 .. automodule:: sxweb
     :members:
     :undoc-members:
--- sx-web/sxweb/__init__.py
+++ sx-web/sxweb/__init__.py
@@ -1,3 +1,40 @@
+# Copyright (c) 2008-2009, samurai-x.org
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the samurai-x.org nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY SAMURAI-X.ORG ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL SAMURAI-X.ORG  BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+    sx-web is a plugin that runs a wsgi server inside samurai-x. 
+
+    Configuration
+    -------------
+
+    .. attribute:: web.port
+    
+        Port number to run the web server on 
+
+"""
+
 import os
 import socket
 import wsgiref
@@ -65,7 +102,6 @@ class WSGIApp(object):
 
 
 class SXWeb(Plugin):
-
     key = 'web'
 
     def __init__(self, app):
@@ -90,4 +126,7 @@ class SXWeb(Plugin):
         print dir(self.httpd)
 
         app.add_fd_handler('read', self.httpd.socket, self.httpd.handle_request)
+
+    def __del__(self):
+        log.debug('sxweb going down!')
 
