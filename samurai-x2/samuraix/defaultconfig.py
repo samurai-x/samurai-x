3a3f210702aeb2cdbe9c052bc8216be2ef1fabfe
Friedrich Weber <fred@samurai-x.org>
2009-06-14 13:59:04 +0200

    modified defaultconfig

--- samurai-x2/samuraix/defaultconfig.py
+++ samurai-x2/samuraix/defaultconfig.py
@@ -26,50 +26,101 @@
     This module just contains a default example configuration that will be
     used as fallback if there is no user configuration found.
     You can print this file using `sx-wm --default-config`.
-    Currently, it attempts to load all standard plugins, defines
-    two floating desktops, creates some hotkeys and binds the
-    left mouse button to the move action, the right mouse button
-    to the resize action.
 """
 
 import logging
 log = logging.getLogger(__name__)
 
 config = {
+    # The search paths for plugin packages
     'core.plugin_paths': ['~/.samuraix/plugins'],
+    # The list of plugins to load. Sometimes, the order is important!
     'core.plugins': [
         'sxactions',
         'sxdesktops',
+        'sxlayoutmgr',
         'sxbind',
         'yahiko_decorator',
         'sxmoveresize',
         'sxclientbuttons',
         'sxfocus',
-        'sxlayoutmgr',
+        'sxhelp',
     ],
 
+    # Here we create 10 desktops. You can find more
+    # information about this in the sx-desktops documentation:
+    # http://docs.samurai-x.org/samurai-x2/plugins/sx-desktops.html#configuration
     'desktops.desktops':
-        [('one', {'layout': 'floating'}),
-         ('two', {'layout': 'vert'})]
+        [
+         ('1', {'layout': 'floating'}),
+         ('2', {'layout': 'floating'}),
+         ('3', {'layout': 'floating'}),
+         ('4', {'layout': 'floating'}),
+         ('5', {'layout': 'floating'}),
+         ('6', {'layout': 'floating'}),
+         ('7', {'layout': 'floating'}),
+         ('8', {'layout': 'floating'}),
+         ('9', {'layout': 'floating'}),
+         ('10', {'layout': 'floating'}),
+         ]
     ,
+    # Here we can bind keystrokes to actions using the sx-bind plugin.
+    # More information about actions:
+    # http://docs.samurai-x.org/samurai-x2/plugins/sx-actions.html
+    # More information about sx-bind: 
+    # http://docs.samurai-x.org/samurai-x2/plugins/sx-bind.html
     'bind.keys': {
             'Meta+n': 'desktops.cycle offset=1',
-            'Meta+m': 'mark',
             'Meta+p': 'desktops.cycle offset=-1',
-            'Meta+c': 'desktops.cycle_clients',
-            'Meta+d': 'log message="pressed d"',
-            'META+X': 'spawn cmdline="xterm"',
-            'META+g': 'spawn cmdline="gimp"',
+            'meta+X': 'spawn cmdline="xterm"',
             'Meta+Q': 'quit',
-            'Meta+R': 'restart',
+            # and shortcuts to reach an arbitrary desktop very fast
+            'Meta+1': 'desktops.goto index=1',
+            'Meta+2': 'desktops.goto index=2',
+            'Meta+3': 'desktops.goto index=3',
+            'Meta+4': 'desktops.goto index=4',
+            'Meta+5': 'desktops.goto index=5',
+            'Meta+6': 'desktops.goto index=6',
+            'Meta+7': 'desktops.goto index=7',
+            'Meta+8': 'desktops.goto index=8',
+            'Meta+9': 'desktops.goto index=9',
+            'Meta+0': 'desktops.goto index=10',
         },
+
+    # That is the configuration for the sx-clientbuttons plugin.
+    # It makes it possible to press the Meta key and the left
+    # mouse button inside a window to move it, and
+    # Meta + right mouse button to resize it.
+    # More information: http://docs.samurai-x.org/samurai-x2/plugins/sx-clientbuttons.html
     'clientbuttons.bindings': {
             'Meta+1': 'moveresize.move',
             'Meta+3': 'moveresize.resize',
         },
+    
+    # Configuration for yahiko_decorator follows.
+    # If you click and drag the titlebar with the left mouse button,
+    # you move the window. If you click and drag with the right mouse
+    # button, you resize it.
     'decorator.title.bindings': {
             '1': 'moveresize.move',
+            '3': 'moveresize.resize',
     },
+
+    # Here we create a "Close" button on the right side of the titlebar.
+    'decorator.buttons.rightside': [
+        {
+            'text': 'X',
+            'width': 20,
+            'style': {
+                'text.color': (1.0, 1.0, 1.0),
+            },
+            # if you click with the left mouse button, the "kill" action
+            # is emitted.
+            'bindings': {
+                '1': 'kill',
+            }
+        }
+    ]
 }
 
 
