7251cc96244746b2259fbcb8d6f997fc4f3c082f
Friedrich Weber <fred@reichbier.de>
2010-06-09 00:07:16 +0200

    Fixed copyright.

--- ooxcb/ooxcb/keysyms.py
+++ ooxcb/ooxcb/keysyms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2008-2009, samurai-x.org
+# Copyright (c) 2008-2010, samurai-x.org
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
